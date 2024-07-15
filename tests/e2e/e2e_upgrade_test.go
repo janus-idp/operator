247a2bcfb6c385c8bfd83962781089fbe5a0ba07
Armel Soro <asoro@redhat.com>
2024-07-15 12:12:45 +0200

    Fix the E2E tests using the latest stable image of the operand
    User authentication is now needed to query the '/api/dynamic-plugins-info/loaded-plugins' API endpoint

--- tests/e2e/e2e_upgrade_test.go
+++ tests/e2e/e2e_upgrade_test.go
@@ -93,7 +93,7 @@ var _ = Describe("Operator upgrade with existing instances", func() {
 			})
 
 			By("checking the status of the existing CR")
-			Eventually(helper.VerifyBackstageCRStatus, time.Minute, time.Second).WithArguments(ns, crName, `"reason":"Deployed"`).Should(Succeed())
+			Eventually(helper.VerifyBackstageCRStatus, 5*time.Minute, time.Second).WithArguments(ns, crName, `"reason":"Deployed"`).Should(Succeed())
 
 			By("checking the Backstage operand pod")
 			Eventually(func(g Gomega) {
